41621f928fdcc9b3877968d0e79b111f5620fcc7
Tạ Quang Trung <76809187+taquangtrung115@users.noreply.github.com>
2022-11-17 20:50:35 +0700

    day 3

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connection from "./config/connectDB";
 
 require('dotenv').config();
 
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+connection();
 
 let port = process.env.PORT || 8000;
 
